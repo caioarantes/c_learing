419757af1aeeeb614a54d29a03aad41208140b59
Caio Arantes <c168343@dac.unicamp.br>
2021-03-14 17:48:36 -0300

    Create Leibniz_formula_for_pi.cpp
    Leibniz formula for pi in C++
    Refeshing my memory on C++ syntax while celebrating Pi Day 2021.

---
+++ Leibniz_formula_for_pi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{    
+    int iterations;
+    double sum = 0;
+    double pi;
+    
+    cout << "Leibniz formula for pi C++.\n\nEnter number of iterations: ";
+    cin >> iterations;
+	
+    double j = -1;
+    for (double i = 1; i < 2*iterations; i= i+2) {
+    j = -j;
+	sum = sum + j*(1/i);
+	}
+	
+	pi = sum*4;
+	
+	cout << "Pi: ";
+	cout<<fixed<<setprecision(8)<<pi;
+	cout << " with " << iterations << " iteractions.\n";
+	
+    return 0;
+}
